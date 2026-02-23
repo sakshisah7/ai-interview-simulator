9ab79b1ec9c8fb5002c1ba71a4ed879460cb4e41
sakshisah9 <156561427+sakshisah7@users.noreply.github.com>
2026-02-24 02:28:36 +0530

    Added basic Scoring logic

--- frontend/app.py
+++ frontend/app.py
@@ -6,11 +6,16 @@ questions = response.json()
 print("\nAI Interview Simulator\n")
 
 answers = []
+score = 0
 
 for q in questions:
     print(f"\nQuestion {q['id']}: {q['question']}")
     user_answer = input("Your Answer: ")
-    
+
+    # Simple scoring logic
+    if len(user_answer.split()) > 3:
+        score += 1
+
     answers.append({
         "question": q["question"],
         "answer": user_answer
@@ -22,3 +27,12 @@ print("Your Answers Summary:\n")
 for i, item in enumerate(answers, start=1):
     print(f"{i}. {item['question']}")
     print(f"   Your Answer: {item['answer']}\n")
+
+print(f"Final Score: {score}/{len(questions)}")
+
+if score == len(questions):
+    print("Excellent performance! 🌟")
+elif score >= 2:
+    print("Good job! Keep improving 👍")
+else:
+    print("You need to elaborate more in your answers.")
